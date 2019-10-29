1de63abe6fbf1cea284dd3555af16c0040a98b3e
dg1an3 <dglane001@gmail.com>
2019-10-29 17:02:46 -0500

    1 file modified

--- ImageStrip.js
+++ ImageStrip.js
@@ -8,11 +8,21 @@ class ImageStrip extends Component {
       <div>
         <h3>Current entries:</h3>
         <BasicFilm>
-          <img alt="Cat 01" src="https://raw.githubusercontent.com/dg1an3/dg1an3.github.io/master/lilly_goof.png?raw=true" style={{height:200}} />
-          <img alt="Cat 02" src="https://raw.githubusercontent.com/dg1an3/dg1an3.github.io/master/lilly_chimkin.png?raw=true"
-          style={{height:200}} />
-          <img alt="Cat 03" src="https://raw.githubusercontent.com/dg1an3/dg1an3.github.io/master/lilly_sleepy.png?raw=true"
-          style={{height:200}} />
+          <img
+            alt="Cat 01"
+            src="https://raw.githubusercontent.com/dg1an3/dg1an3.github.io/master/lilly_goof.png?raw=true"
+            style={{ height: 200 }}
+          />
+          <img
+            alt="Cat 02"
+            src="https://raw.githubusercontent.com/dg1an3/dg1an3.github.io/master/lilly_chimkin.png?raw=true"
+            style={{ height: 200 }}
+          />
+          <img
+            alt="Cat 03"
+            src="https://raw.githubusercontent.com/dg1an3/dg1an3.github.io/master/lilly_sleepy.png?raw=true"
+            style={{ height: 200 }}
+          />
         </BasicFilm>
       </div>
     );
